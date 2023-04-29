9e5f6762afc5307941092ebcebff3389d8519718
Chetan Bawankule <chetanbawankule2000@gmail.com>
2023-04-29 17:43:49 +0530

    small change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-  import { browser } from "$app/environment";
   import { onDestroy, onMount } from "svelte";
 
   let map;
@@ -9,25 +8,23 @@
     const L = await import("leaflet");
     await import("@geoman-io/leaflet-geoman-free");
 
-    if (browser) {
-      map = L.map(mapElement).setView([51.505, -0.09], 13);
+    map = L.map(mapElement).setView([51.505, -0.09], 13);
 
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
-      // Bind the marker
-      L.marker([51.5, -0.09])
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
+    // Bind the marker
+    L.marker([51.5, -0.09])
+      .addTo(map)
+      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+      .openPopup();
 
-      // add Leaflet-Geoman controls
-      map.pm.addControls({
-        position: "topleft",
-      });
-    }
+    // add Leaflet-Geoman controls
+    map.pm.addControls({
+      position: "topleft",
+    });
   });
 
   onDestroy(async () => {
